f2033fb97b8afef56f98bd1ea9a96756dcd63dd2
herman-leung <1872825042@qq.com>
2022-03-23 09:52:22 +0800

    getattr问题

---
+++ getattr.py
@@ -0,0 +1,12 @@
+class Foo:
+    def __getattr__(self, attr):
+        print("你调用了%s" % attr)
+        return self.other
+
+    def other(self, p1, p2):
+        print(f'另外的函数,参数为{p1},{p2}')
+
+
+f = Foo()
+print('hello')
+f.bar(1, 2)
